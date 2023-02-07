39dd855b21232fb653776ad3cad76fda2b887814
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-02-07 18:55:34 +0530

    last update in design
    Signed-off-by: Ujjwal Garg <ujjwalgarg100204@gmail.com>

--- src/components/UI/Step.tsx
+++ src/components/UI/Step.tsx
@@ -7,7 +7,7 @@ type StepUIProp = {
 export default function Step(props: StepUIProp): JSX.Element {
     return (
         <div
-            className={`z-10 sm:m-4 sm:mt-32 sm:rounded-md sm:bg-white sm:px-6 sm:pt-8 sm:pb-12 sm:shadow-xl xl:static xl:relative xl:mt-0 xl:min-w-[55rem] xl:py-10 xl:pl-28 xl:shadow-none ${props.className}`}
+            className={`z-10 sm:m-4 sm:mt-32 sm:rounded-md sm:bg-white sm:px-6 sm:pt-8 sm:pb-12 sm:shadow-xl xl:static xl:relative xl:mt-0 xl:ml-28 xl:min-w-[55rem] xl:py-10 xl:shadow-none ${props.className}`}
         >
             {props.children}
         </div>
